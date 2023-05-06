a46d78e5337412e26de9111e13aa0b0e81246192
kup1o <kup1o@pm.me>
2023-05-06 17:48:52 +0300

    chore(branches): condence the code

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -15,20 +15,17 @@ fn main() {
 
         if number % 4 == 0 {
             println!("number is divisible by 4");
-            if number % 3 == 0 {
-                println!("number is divisible by 3");
-            }
-            if number % 2 == 0 {
-                println!("number is divisible by 2");
-            }
-        } else if number % 3 == 0 {
+        }
+
+        if number % 3 == 0 {
             println!("number is divisible by 3");
-            if number % 2 == 0 {
-                println!("number is divisible by 2");
-            }
-        } else if number % 2 == 0 {
+        }
+
+        if number % 2 == 0 {
             println!("number is divisible by 2");
-        } else {
+        }
+
+        if number % 4 != 0 && number % 3 != 0 && number % 2 != 0 {
             println!("number is not divisible by 4, 3 or 2");
         }
 
