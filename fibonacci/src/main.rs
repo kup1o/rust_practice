af94282e0b7453d3b22026624f28adf14efdf896
kup1o <kup1o@pm.me>
2023-05-07 07:06:49 +0300

    feat(chapter 3, fibonacci): speed up code by using vectors

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -3,28 +3,41 @@ use std::io;
 fn main() {
     loop {
         println!("Fibonacci generator");
-        println!("Note that the first F = 0 is ommited by index.");
-        println!("Write an index of a fibonacci number:");
+        println!("Note that the first F = 0 is omitted by index.");
+        println!("Write an index of a Fibonacci number:");
 
         let mut index = String::new();
         io::stdin()
             .read_line(&mut index)
             .expect("Failed to read the line");
-        let index: u32 = match index.trim().parse() {
+        let index: u128 = match index.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        println!("Your index is {index}");
-        println!("Your fibonnaci number is {}", fibonacci(index));
+
+        if index > 186 {
+            println!("Index over 186 is not currently supported");
+            continue;
+        } else {
+            println!("Your index is {}", index);
+            println!("Your Fibonacci number is {}", fibonacci(index));
+        }
 
         break;
     }
 }
 
-fn fibonacci(n: u32) -> u32 {
-    match n {
-        0 => 1,
-        1 => 1,
-        _ => fibonacci(n - 1) + fibonacci(n - 2),
+fn fibonacci(n: u128) -> u128 {
+    if n <= 1 {
+        return n;
     }
+
+    let mut fib_nums: Vec<u128> = vec![0, 1];
+
+    for i in 2..=n {
+        let fib = fib_nums[(i - 1) as usize] + fib_nums[(i - 2) as usize];
+        fib_nums.push(fib);
+    }
+
+    fib_nums[n as usize].into()
 }
