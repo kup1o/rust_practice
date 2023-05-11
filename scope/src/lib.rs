508ffa1cd131d49e4193a60c23ef221decb78870
kup1o <kup1o@pm.me>
2023-05-11 19:50:28 +0300

    feat(chapter 7): add a scope

---
+++ scope/src/lib.rs
@@ -0,0 +1,24 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+// use only works in the scope
+// use crate::front_of_house::hosting;
+// The easiest way is `pub use`
+pub use crate::front_of_house::hosting;
+
+mod customer {
+    // To fix this problem, move the use within the customer module too
+    use crate::front_of_house::hosting;
+    pub fn eat_at_restaraunt() {
+        // or reference the shortcut in the parent module with super::hosting within the child customer module
+        // super::crate::front_of_house::hosting::add_to_waitlist();
+        hosting::add_to_waitlist(); // L#14
+    }
+}
+
+pub fn eat_at_cafe () {
+    hosting::add_to_waitlist(); // L#10
+}
