a4a2e4e3ed3d3cc89bb400eb94ba6c567a4a065a
kup1o <kup1o@pm.me>
2023-05-13 09:14:16 +0300

    refractor(guessing_game): use a Guess type for input validation This commit refactors the code to improve input validation for the guessing game. Instead of manually checking the input range in multiple functions, a new Guess type is introduced. The Guess struct contains a value field and a new function called new, which ensures that the value is between 1 and 100.
    By using the Guess type and its new function, it becomes easier to enforce the range constraint and eliminate repetitive validation checks throughout the codebase. This approach enhances code clarity and maintainability, making it safer for functions to use the Guess type confidently.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            println!("Guess value must be between 1 and 100");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number from 1 to 100!");
 
@@ -21,16 +39,13 @@ fn main() {
             Err(_) => continue,
         };
 
-        if guess < 1 || guess > 100 {
-            println!("The number will be between 1 and 100.");
-            continue;
-        }
+        let guess = Guess::new(guess);
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!");
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You won!");
                 break;
