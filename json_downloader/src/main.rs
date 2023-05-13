28f1146bc39057ef2e213ab37124281ff6abc15d
kup1o <kup1o@pm.me>
2023-05-13 05:12:03 +0300

    chore: rustfmt

--- json_downloader/src/main.rs
+++ json_downloader/src/main.rs
@@ -1,6 +1,11 @@
-use std::{fs::File, io::{self, Write}};
+use std::{
+    fs::File,
+    io::{self, Write},
+};
 
-static APP_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36";
+static APP_USER_AGENT: &str =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/94.0.4606.61 Safari/537.36";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
