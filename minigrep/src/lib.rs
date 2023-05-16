967849ee9c53c96d6c774f4de8ebad40049dfe99
kup1o <kup1o@pm.me>
2023-05-16 19:59:54 +0300

    fix(minigrep): add forgotten `.to_lowercase()`

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -58,7 +58,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
-        .filter(|line| line.contains(query))
+        .filter(|line| line.to_lowercase().contains(query))
         .collect()
 }
 
